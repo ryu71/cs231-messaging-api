12033fe88c079142dfd423675c017c40ebaba9ab
Raymond Y <ryu71@mail.ccsf.edu>
2019-04-12 23:18:49 -0700

    Update database settings

--- src/drfapi/drfapi/settings.py
+++ src/drfapi/drfapi/settings.py
@@ -90,12 +90,12 @@ ASGI_APPLICATION = 'drfapi.routing.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # This does not work on heroku
 # DATABASES = {
