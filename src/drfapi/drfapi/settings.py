9e220da753dfc649cc8ea16ec31d8383922186c2
Raymond Y <ryu71@mail.ccsf.edu>
2019-04-12 22:20:06 -0700

    Migrate to PostgreSQL database

--- src/drfapi/drfapi/settings.py
+++ src/drfapi/drfapi/settings.py
@@ -88,10 +88,21 @@ ASGI_APPLICATION = 'drfapi.routing.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'messageapp',
+        'USER': 'ryu71',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
